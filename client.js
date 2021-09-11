5be0d102d085e579952c26f5d5f51bff93f8ded9
Matt Sroufe <mattsroufe@gmail.com>
2021-09-12 09:37:16 +1200

    Revert "move frontend stuff to frontend directory"
    This reverts commit a83283705d9d6dc2350d6d8f02514f4b51691865.

---
+++ client.js
@@ -0,0 +1,119 @@
+const MESSAGE_RATE_MILLISECONDS = 200
+
+var pc = null;
+
+// data channel
+var dc = null, dcInterval = null;
+
+function negotiate() {
+    pc.addTransceiver('video', {direction: 'recvonly'});
+    pc.addTransceiver('audio', {direction: 'recvonly'});
+    return pc.createOffer().then(function(offer) {
+        return pc.setLocalDescription(offer);
+    }).then(function() {
+        // wait for ICE gathering to complete
+        return new Promise(function(resolve) {
+            if (pc.iceGatheringState === 'complete') {
+                resolve();
+            } else {
+                function checkState() {
+                    if (pc.iceGatheringState === 'complete') {
+                        pc.removeEventListener('icegatheringstatechange', checkState);
+                        resolve();
+                    }
+                }
+                pc.addEventListener('icegatheringstatechange', checkState);
+            }
+        });
+    }).then(function() {
+        var offer = pc.localDescription;
+        return fetch('/offer', {
+            body: JSON.stringify({
+                sdp: offer.sdp,
+                type: offer.type,
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'POST'
+        });
+    }).then(function(response) {
+        return response.json();
+    }).then(function(answer) {
+        return pc.setRemoteDescription(answer);
+    }).catch(function(e) {
+        alert(e);
+    });
+}
+
+function start() {
+    var config = {
+        sdpSemantics: 'unified-plan'
+    };
+
+    if (document.getElementById('use-stun').checked) {
+        config.iceServers = [{urls: ['stun:stun.l.google.com:19302']}];
+    }
+
+    pc = new RTCPeerConnection(config);
+
+    var time_start = null;
+
+    function current_stamp() {
+        if (time_start === null) {
+            time_start = new Date().getTime();
+            return 0;
+        } else {
+            return new Date().getTime() - time_start;
+        }
+    }
+
+    // connect audio / video
+    pc.addEventListener('track', function(evt) {
+        if (evt.track.kind == 'video') {
+            document.getElementById('video').srcObject = evt.streams[0];
+        } else {
+            document.getElementById('audio').srcObject = evt.streams[0];
+        }
+    });
+
+    // <option value='{"ordered": true}'>Ordered, reliable</option>
+    //  <option value='{"ordered": false, "maxRetransmits": 0}'>Unordered, no retransmissions</option>
+    // <option value='{"ordered": false, "maxPacketLifetime": 500}'>Unordered
+
+    var dc = pc.createDataChannel('chat', {"ordered": true});
+    dc.onclose = function() {
+      clearInterval(dcInterval);
+      console.log('data close');
+    };
+    dc.onopen = function() {
+      console.log('data open');
+      dcInterval = setInterval(function() {
+        for (const pad of navigator.getGamepads()) {
+          dc.send(JSON.stringify({
+            axes: pad.axes,
+            buttons: pad.buttons.map(function(button) {
+              return button.value
+            })
+          }));
+        }
+      }, MESSAGE_RATE_MILLISECONDS);
+    };
+    dc.onmessage = function(evt) {
+      console.log(evt.data);
+    };
+
+    document.getElementById('start').style.display = 'none';
+    negotiate();
+    document.getElementById('stop').style.display = 'inline-block';
+}
+
+function stop() {
+    document.getElementById('stop').style.display = 'none';
+
+    // close peer connection
+    setTimeout(function() {
+        pc.close();
+    }, 500);
+}
+
